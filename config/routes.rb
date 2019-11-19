c1423da6aa0c18ff765cf95e87e8139e55354b47
Joe Paul <joe.paul0712@gmail.com>
2019-11-19 20:11:14 +0000

    finishing routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
     resources :comments, only: [:create, :edit, :destroy]
   end
   resources :users, only: [:show, :edit, :update, :destroy] do
-    resources :jokes, only: [:show, :edit, :update]
+    resources :favourites, only: [:show, :edit, :update]
   end
+  resources :comments, only: [:show, :edit, :update, :destroy]
+  resources :favourites, only: [:destroy, :index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
