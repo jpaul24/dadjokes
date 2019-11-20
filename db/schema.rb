8ea5624b00408874b74740d9cd3d3a50e8ce565d
Joe Paul <joe.paul0712@gmail.com>
2019-11-20 15:40:41 +0000

    completed foreign keys

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_20_151711) do
+ActiveRecord::Schema.define(version: 2019_11_20_154012) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(version: 2019_11_20_151711) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.bigint "joke_id"
+    t.index ["joke_id"], name: "index_favourites_on_joke_id"
     t.index ["user_id"], name: "index_favourites_on_user_id"
   end
 
@@ -57,6 +59,7 @@ ActiveRecord::Schema.define(version: 2019_11_20_151711) do
 
   add_foreign_key "comments", "jokes"
   add_foreign_key "comments", "users"
+  add_foreign_key "favourites", "jokes"
   add_foreign_key "favourites", "users"
   add_foreign_key "jokes", "users"
 end
