745a79b3d33fc16a77390df0b90b0743ef99b6ba
Joe Paul <joe.paul0712@gmail.com>
2019-11-21 20:33:25 +0000

    comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,4 +2,12 @@ class CommentsController < ApplicationController
 
   private
 
+  def set_comment
+    @comment = Comment.find(params[:id])
+    authorize @comment
+  end
+
+  def comment_params
+    params.require(:comment).permit(:comment, :user_id, :garden_id)
+  end
 end
