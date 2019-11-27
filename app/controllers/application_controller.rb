0ed72b2c6e2506c2f8d201b6214e8b1bb6bc12f4
Joe Paul <joe.paul0712@gmail.com>
2019-11-27 17:38:58 +0000

    working on the jokes controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,14 @@
 class ApplicationController < ActionController::Base
+  # include Pundit
   protect_from_forgery with: :exception
+
   before_action :authenticate_user!
-  before_action :configure_permitted_parameters, if: :devise_controller?
-  include Pundit
+
+  # before_action :configure_permitted_parameters, if: :devise_controller?
 
   # Pundit: white-list approach.
-  after_action :verify_authorized, except: :index, unless: :skip_pundit?
-  after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
+  # after_action :verify_authorized, except: :index, unless: :skip_pundit?
+  # after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
 
   # Uncomment when you *really understand* Pundit!
   # rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
@@ -21,9 +23,9 @@ class ApplicationController < ActionController::Base
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
   end
 
-  protected
+  # protected
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
-  end
+  # def configure_permitted_parameters
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+  # end
 end
--- app/controllers/jokes_controller.rb
+++ app/controllers/jokes_controller.rb
@@ -1,12 +1,21 @@
 class JokesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
+  before_action :set_joke, only: [:show, :edit, :update, :destroy]
 
   def index
+    @jokes = policy_scope(Joke)
   end
 
   def show
   end
 
   def new
+  end
+
+  private
+
+  def set_joke
+    @joke = Joke.find(params[:id])
+    authorize @joke
   end
 end
