a6a311017d16055f8388e657267ecd31a59f3ea5
Joe Paul <joe.paul0712@gmail.com>
2019-12-02 14:18:10 +0000

    user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,20 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update]
+
   def edit
   end
-
   def show
+    @user = User.find(params[:id])
+    authorize @user
+  end
+
+  private
+
+  def set_user
+    @user = current_user
+  end
+
+  def user_params
+    params.require(:user).permit(:username)
   end
 end
