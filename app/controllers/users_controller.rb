d7d77b01f328bd060a8acaadba5fe823b4400dfc
Joe Paul <joe.paul0712@gmail.com>
2019-12-04 19:37:00 +0000

    finally finishing the controller for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,29 @@ class UsersController < ApplicationController
     authorize @user
   end
 
+  def edit
+    authorize @user
+  end
+
+  def update
+    authorize @user
+    if @user.update(user_params)
+      redirect_to user_path(current_user)
+    else
+      render :edit
+    end
+  end
+
+  def comments
+    @comments = current_user.comments
+    authorize @comments
+  end
+
+  def favourites
+    @favourites = current_user.favourites
+    authorize @favourites
+  end
+
   private
 
   def set_user
