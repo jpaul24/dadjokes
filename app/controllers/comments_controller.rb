4924a8d9262288f54d1f817da93baa58cab589e1
Joe Paul <joe.paul0712@gmail.com>
2019-12-06 15:39:05 +0000

    adding to create in the comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,7 +6,16 @@ class CommentsController < ApplicationController
   end
 
   def create
+    @comment = Comment.new(comment_params)
+    @joke = Joke.find(params[:joke_id].to_i)
+    @comment.joke = @joke
+    @comment.user = current_user
+    authorize @comment
+    if @comment.save
 
+    else
+      render :new
+    end
   end
 
   private
