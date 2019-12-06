6ea95f5009ac84945d6dc7a7008d8ec6eff97ea1
Joe Paul <joe.paul0712@gmail.com>
2019-12-06 15:58:16 +0000

    seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Joke.destroy_all
+
 puts "creating users"
-luke = User.create!()
+luke = User.create!(email: 'marc@gmail.com', password:'mypassword', username: 'ldawg')
 puts "users created"
 
 puts "creating jokes"
+joke1 = Joke.create!(user: luke, joke: 'Man walks into a bar, ouch')
 puts "jokes created"
