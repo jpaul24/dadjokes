c9ddd0c27409110d182f1a58ffa35b16fad88387
Joe Paul <joe.paul0712@gmail.com>
2019-12-06 15:35:21 +0000

    comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,17 @@
 class CommentsController < ApplicationController
+
   def new
-    @comment = Review.new
+    @comment = Comment.new
     authorize @comment
+  end
+
+  def create
+
   end
 
   private
 
   def comment_params
-    params.require(:comment).permit(:joke_id, :user_id)
+    params.require(:comment).permit(:comment, :joke_id, :user_id)
   end
 end
